5f7386ae5d40a96aed8da09907181013257b7c25
Daniel Gomes de Lima <50140741+dnlgomesl@users.noreply.github.com>
2022-01-22 20:43:16 -0300

    addiciona navbar

---
+++ src/scripts/mobile-navbar.js
@@ -0,0 +1,20 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const navList = document.querySelector(".nav-list");
+const navLinks = document.querySelectorAll(".nav-list li");
+const activeClass = "active";
+  
+function animateLinks() {
+      navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+}
+  
+function handleClick() {
+      navList.classList.toggle(activeClass);
+      mobileMenu.classList.toggle(activeClass);
+      animateLinks();
+    }
